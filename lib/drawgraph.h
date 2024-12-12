0343a663e088b375be8f5ac39d9f654e34e2e7ae
stefcosmin <cosmin.stefanache2005@outlook.com>
2024-12-12 10:38:28 +0200

    pan

--- lib/drawgraph.h
+++ lib/drawgraph.h
@@ -50,10 +50,15 @@ void PanZoom()
 {
   int x = 0, y = 0;
   int midx = getmaxx() - getmaxx() / 8, midy = getmaxy() / 2;
-  float center = (inf + sup) / 2;
-  float half_width = (sup - inf) / 2;
-  while (!((x > (midx + 50) && x < (midx + 100) && y > (midy + 200) && y < (midy + 250)) || // plus
-           (x > (midx + 50) && x < (midx + 100)) && y > (midy + 275) && y < (midy + 325)))  // minus
+
+  float center = (inf + sup) / 2; // var de calcul
+  float dif = sup - inf;
+  float half_width = dif / 2;
+
+  while (!((x > (midx + 50) && x < (midx + 100) && y > (midy + 200) && y < (midy + 250)) ||   // plus
+           ((x > (midx + 50) && x < (midx + 100)) && y > (midy + 275) && y < (midy + 325)) || // minus
+           (x > (midx - 90) && x < (midx - 40) && y > (midy) && y < (midy + 50)) ||           // stanga
+           (x > (midx + 50) && x < (midx + 100) && y > (midy) && y < (midy + 50))))           // dreapta
   {
 
     while (!ismouseclick(WM_LBUTTONDOWN))
@@ -61,16 +66,27 @@ void PanZoom()
     getmouseclick(WM_LBUTTONDOWN, x, y);
   }
 
-  if (x > (midx + 50) && x < (midx + 100) && y > (midy + 200) && y < (midy + 250))
+  if (x > (midx + 50) && x < (midx + 100) && y > (midy + 200) && y < (midy + 250)) // minus
   {
     inf = center - (0.5 * half_width);
     sup = center + (0.5 * half_width);
   }
-  else if (x > (midx + 50) && x < (midx + 100) && y > (midy + 275) && y < (midy + 325))
+  else if (x > (midx + 50) && x < (midx + 100) && y > (midy + 275) && y < (midy + 325)) // plus
   {
 
     inf = center - (2 * half_width);
     sup = center + (2 * half_width);
   }
+  else if (x > (midx - 90) && x < (midx - 40) && y > (midy) && y < (midy + 50)) // stanga
+  {
+    inf -= dif / 8;
+    sup -= dif / 8;
+  }
+  else if (x > (midx + 50) && x < (midx + 100) && y > (midy) && y < (midy + 50)) // dreapta
+  {
+    inf += dif / 8;
+    sup += dif / 8;
+  }
+
   drawFun();
 }
